bce0bdf7bee47f4b193d76297c53377477d08a02
jpatena <patenaj@coyote.csusb.edu>
2018-04-20 20:40:39 -0700

    Loop ends with 6 misses.

--- hangman.py
+++ hangman.py
@@ -6,15 +6,26 @@ correctWord = list("chicken")
 guessWord = []
 for j in range(0, len(correctWord)):
 	guessWord.insert(j, '-')
+miss = 0
 
-while True:
+while miss < 6:
 	userInput = raw_input('Enter: ')
+
+	gotItRight = False
 
 	# iterate the string
 	for i in range(0, len(correctWord)):
-		# if userinput is in each letter of word
+		# if userinput is in each char of word
 		if correctWord[i] == userInput:
 			del guessWord[i]
 			guessWord.insert(i, userInput)
 
+			# flag when guessed right
+			gotItRight = True
+
+	# increment when guessed wrong
+	if gotItRight == False:
+		miss = miss + 1
+
+	print miss
 	print " ".join(guessWord)
